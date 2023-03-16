7c03a44337ecf2ca5cd70f4bf8e6c3e3515198ff
johnmuthua <caveomuthama@gmail.com>
2023-03-16 14:20:47 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Description: 
-* This program prints "Programming is like building a multilingual puzzle
-* Ends with a 0 if executed succesfully
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
