2eadd3473f865a6adccf198d91d3fda127c4d19c
johnmuthua <caveomuthama@gmail.com>
2023-03-21 15:39:47 -0700

    add 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include 'main.h'
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the initial value
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	if (c > 0 || c == 0)
+	{
+		_putchar(c * 10 + '0');
+		return (c % 10);
+	}
+	else
+	{
+		c *= -1;
+		_putchar(c * 10 + '0);
+		return (c % 0);
+	}
+}
