7dd05efeb06aef6aa812c72365ca52f279a3eb7b
johnmuthua <caveomuthama@gmail.com>
2023-03-21 14:52:02 -0700

    added 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,15 +6,13 @@
  */
 int print_last_digit(int c)
 {
-	if (c > 0 || c == 0)
-	{
-		_putchar(c % 10 + '0');
-		return (c % 10);
-	}
+	int last;
+
+	if (c < 0)
+		last = (c % 10) * -1;
 	else
-	{
-		c *= -1;
-		_putchar(c % 10 + '0');
-		return (c % 10);
-	}
+		last = c % 10;
+	_putchar(last + '0');
+
+	return (last);
 }
