1c610073f62247b2b1f6a821abd60a37b8e87c4c
johnmuthua <caveomuthama@gmail.com>
2023-03-22 02:25:29 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,6 +22,12 @@ void print_times_table(int n)
 			{
 				_putchar(' ');
 				_putchar(' ');
+				_putchar('0' + val);
+			}
+			else if (val < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + val / 10);
 				_putchar('0' + val % 10);
 			}
 			else
