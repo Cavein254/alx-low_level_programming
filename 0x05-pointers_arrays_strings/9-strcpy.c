834536c4f5b24083aacfdc5a0d699d6e1bf09d0a
John Muthua <caveomuthama@gmail.com>
2023-03-28 02:33:10 +0300

    Add 09

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - prints n elements of an array of integers
+ * @src: The original array
+ * @dest: The array being copyed
+ *
+ * Return: character pointer to the string
+ */
+char *_strcpy(char *dest, char *src)
+{
+    char *pdest = dest;
+    char *psrc = src;
+    int i;
+
+    for (i = 0; psrc[i] != '\0'; i ++)
+    {
+        pdest[i] = psrc[i]; 
+    }
+    
+    return pdest;
+}
+
+
