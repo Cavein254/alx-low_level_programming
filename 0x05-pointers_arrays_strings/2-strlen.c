0447f45dbee46c854c79b0f3ee4dfdd514231b52
johnmuthua <caveomuthama@gmail.com>
2023-03-27 09:42:26 -0700

    add 02

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - determines the length of the string
+ * @s: variable containing the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	char *ps = s;
+	int count;
+	int length = 0;
+
+	for (count = 0; ps[count] != '\n'; count++)
+	{
+		length++;
+	}
+
+	return  (count);
+}
