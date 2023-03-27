56587e4e5d9d0c0f580458d37608f61f80b5e61c
John Muthua <caveomuthama@gmail.com>
2023-03-27 17:00:13 +0300

    added 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - takes input and changes it to 98
+* @n : The provided integer
+* Return : This function does not return
+*/
+void reset_to_98(int *n)
+{
+    int *p = &n;
+    *p = 402;
+}
