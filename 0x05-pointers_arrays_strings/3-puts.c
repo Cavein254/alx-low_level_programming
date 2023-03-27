41ae239dfc01e6b3d32d4bf8e6c9c654f2acd055
johnmuthua <caveomuthama@gmail.com>
2023-03-27 12:58:20 -0700

    added 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,10 +7,11 @@
 void _puts(char *str)
 {
 	char *ps = str;
-	int i;
-	for (i = 0; ps[i] != '\n'; i++)
+	
+	while (*ps != '\0')
 	{
-		_putchar(ps[i]);
+		_putchar(*ps);
+		*ps++;
 	}
 	_putchar('\n');
 }
