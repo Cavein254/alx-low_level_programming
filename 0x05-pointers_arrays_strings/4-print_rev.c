d09f0d5c5ae74d0975ed489e8ea9175ffe82e494
johnmuthua <caveomuthama@gmail.com>
2023-03-27 13:04:26 -0700

    Added 04

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,18 @@
  */
 void print_rev(char *s)
 {
-    char *pstr = s;
-    int counter = 0;
-    int i;
-    int j;
+	char *pstr = s;
+	int counter = 0;
+	int i;
+	int j;
 
-    for (i = 0; pstr[i] != '\0'; i++)
-    {
-        counter++;
-    }
-
-    for (j = counter - 3; j >= 0; j--)
-    {
-        _putchar(pstr[j]);
-    }
-    _putchar('\n');
+	for (i = 0; pstr[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	for (j = counter - 3; j >= 0; j--)
+	{
+		_putchar(pstr[j]);
+	}
+	_putchar('\n');
 }
