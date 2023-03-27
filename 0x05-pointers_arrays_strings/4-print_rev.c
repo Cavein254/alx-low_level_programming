d5f2a844e2c9378626acd5215d1ecd0389df60e7
John Muthua <caveomuthama@gmail.com>
2023-03-27 22:57:11 +0300

    Add 04

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - A function to reverse a string
+ * @s: A string
+ * Return: This function does not return
+ */
+void print_rev(char *s)
+{
+    char *pstr = s;
+    int counter = 0;
+    int i;
+    int j;
+
+    for (i = 0; pstr[i] != '\0'; i++)
+    {
+        counter++;
+    }
+
+    for (j = counter - 3; j >= 0; j--)
+    {
+        _putchar(pstr[j]);
+    }
+    _putchar('\n');
+}
