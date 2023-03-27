876877caef4895c9ba5da76fecce676780197832
John Muthua <caveomuthama@gmail.com>
2023-03-27 23:46:05 +0300

    add 06

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - skips one character and prints the other
+ * @s: variable containing the string
+ *
+ * Return: This function does not return
+ */
+void puts2(char *str)
+{
+    char *pstr = str;
+    int i;
+
+    for (i = 0; pstr[i] != '\0'; i++)
+    {
+        if (i % 2 == 0)
+        {
+            _putchar(pstr[i]);
+        }
+    }
+    _putchar('\n');
+}
