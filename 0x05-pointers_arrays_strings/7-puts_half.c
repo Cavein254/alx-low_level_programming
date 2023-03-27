2b4737d76cf2d51432a96deb2e83604d15b0770d
johnmuthua <caveomuthama@gmail.com>
2023-03-27 14:15:03 -0700

    Add 07 betty

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,30 +8,29 @@
  */
 void puts_half(char *str)
 {
-    char *sptr = str;
-    int i, j, k;
-    int counter = 0;
+	char *sptr = str;
+	int i, j, k;
+	int counter = 0;
 
-    for (i = 0; sptr[i] != '\0'; i++)
-    {
-        counter++;
-    }
-
-    if (counter % 2 == 0)
-    {
-        for (j = (counter / 2); j <= counter; j++)
-        {
-            _putchar(sptr[j]);
-        }
-        _putchar('\n');
-    }
-    else
-    {
-        for (k = ((counter + 1) / 2); k <= counter; k++)
-        {
-            _putchar(sptr[k]);
-        }
-        _putchar('\n');
-    }
-    printf("counter %d", counter);
+	for (i = 0; sptr[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	if (counter % 2 == 0)
+	{
+		for (j = (counter / 2); j <= counter; j++)
+		{
+			_putchar(sptr[j]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (k = ((counter + 1) / 2); k <= counter; k++)
+		{
+			_putchar(sptr[k]);
+		}
+		_putchar('\n');
+	}
+	printf("counter %d", counter);
 }
