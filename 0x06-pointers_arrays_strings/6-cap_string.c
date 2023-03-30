442241e592a7f2fe64c7944f0c522476042b6ff0
johnmuthua <caveomuthama@gmail.com>
2023-03-30 11:43:13 -0700

    Add 6ss

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,9 +10,12 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (!(str[i] >= 'a' && str[i] <= 'z'))
+	while (str[i])
 	{
-		i++;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			i++;
+		}
 		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
 			str[i - 1] == '\n' ||
@@ -30,7 +33,7 @@ char *cap_string(char *str)
 		{
 			str[i] -= 32;
 		}
-		i++;
+			i++;
 	}
 	return (str);
 }
