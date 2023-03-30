f1c33d887f37d9cf2fa2b5865999733184b33d39
johnmuthua <caveomuthama@gmail.com>
2023-03-30 11:53:54 -0700

    4 betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,7 @@
 /**
  * reverse_array - reverses the content of an array of integers
  * @a: The first string
- * @b: The second string
+ * @n: The second string
  *
  * Return: this fn does not return
  */
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strncpy(char *dest, char *src, int n);
 char *leet(char *);
 char *cap_string(char *);
 char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
