f90b3f6fd324b230b90821c342f219f256480197
johnmuthua <caveomuthama@gmail.com>
2023-04-03 04:38:56 -0700

    Add 00 twice

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,11 +3,11 @@
  * _memset - fills the first n bytes of the memory area
  * pointed to by s with the constant byte b
  *
- * @s : The pointed memory area
- * @b : The constant byte to fill the memory area
- * @n : The memory area to be filled
+ * @s:The pointed memory area
+ * @b:The constant byte to fill the memory area
+ * @n:The memory area to be filled
  *
- * Return : A pointer to the value s
+ * Return:A pointer to the value s
  *
  */
 char *_memset(char *s, char b, unsigned int n)
