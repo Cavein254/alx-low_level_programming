c70a7d44f62c5510e42d76b4de77d71057c5058a
John Muthua <caveomuthama@gmail.com>
2023-04-10 22:52:59 +0300

    _isdigit

---
+++ 0x09-static_libraries/_isalpha.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** _isalpha - checks if characters are uppercase
+ * @c : Takes in characters
+ * Return: 0 on success
+*/
+int _isalpha(int c)
+{
+    return (0);
+}
---
+++ 0x09-static_libraries/_isupper.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** _isupper - takes an interger
+ * @c : Takes in an integer
+ * Return: 0 on success
+*/
+int _isupper(int c)
+{
+    return (0);
+}
