894b6d276a1a77bcc9ef2e3c7fb11443dfe4903a
johnmuthua <caveomuthama@gmail.com>
2023-04-12 16:54:25 -0700

    addded2022

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,52 +8,42 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char* comb;
+	char *comb;
 	int x, y;
 
 	x = 0;
 	y = 0;
-
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	while (s1[x] != '\0')
 	{
 		x++;
 	}
-
 	while (s2[y] != '\0')
 	{
 		y++;
 	}
-
 	comb = malloc(sizeof(char) * (x + y + 1));
-
 	if (comb == NULL)
 	{
 		return (NULL);
 	}
-	
 	x = y = 0;
-
 	while (s1[x] != '\0')
 	{
 		comb[x] = s1[x];
 		x++;
 	}
-
 	while (s2[y] != '\0')
 	{
 		comb[x] = s2[y];
-		x++;
-		y++;
+		x++, y++;
 	}
 	comb[x] = '\0';
 	return (comb);
