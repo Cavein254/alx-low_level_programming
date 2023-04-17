515fdb10ce386ffbaa45bd9acb67e6e4d73ea580
johnmuthua <caveomuthama@gmail.com>
2023-04-17 09:38:12 -0700

    betty dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,15 +2,15 @@
 #define MY_DOG_H
 /**
  * struct dog - Properties of a dog
- * @*name: The name of the dog
- * @*owner: who it belongs to
+ * @name: The name of the dog
+ * @owner: who it belongs to
  * @age: how old it is
  */
 struct dog
 {
-    char *name;
-    char *owner;
-    float age;
+	char *name;
+	char *owner;
+	float age;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
