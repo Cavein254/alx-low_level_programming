ecc52cdbbd3c1f76cc2ac35762d3f155ccc09cb0
John Muthua <caveomuthama@gmail.com>
2023-04-17 11:10:22 +0300

    Add 001

---
+++ dog.h
@@ -0,0 +1,16 @@
+#ifndef MY_DOG_H
+#define MY_DOG_H
+/**
+ * struct dog - Properties of a dog
+ * @*name: The name of the dog
+ * @*owner: who it belongs to
+ * @age: how old it is
+ */
+struct dog
+{
+    char *name;
+    char *owner;
+    float age;
+};
+
+#endif
