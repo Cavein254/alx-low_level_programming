4381c573a50c74676ad716b20ebb5fd8f085e6c0
johnmuthua <caveomuthama@gmail.com>
2023-05-01 11:43:02 -0700

    Add 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * listint_len - a function that returns length of a linked list
+ * @h: the first part of a linked list
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != 0)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
