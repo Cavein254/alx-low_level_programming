1695799e3ddef94d37bff297560f96e7aa054132
johnmuthua <caveomuthama@gmail.com>
2023-06-08 04:11:23 +0300

    Add 1-print_rev_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "header.h"
+/**
+ * _puts_recursion - cycles through a sentence and prints characters
+ * *s: pointer to the value
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putschar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s+1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,5 @@
+#ifndef Header_h_
+#define Header_h_
+void _puts_recursion(char *s);
+int _putchar(char c);
+#endif
