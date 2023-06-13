6dc51085b0d894d284c394ec7c6b5c5e253aae21
johnmuthua <caveomuthama@gmail.com>
2023-06-13 13:48:05 +0300

    Corrected betty errors

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,17 +12,15 @@ int **alloc_grid(int width, int height)
 {
 	int **gridout;
 	int i, j;
-	
+
 	if (width < 1 || height < 1)
 		return (NULL);
 	gridout = malloc(height * sizeof(int *));
-	
 	if (gridout == NULL)
 	{
 		free(gridout);
 		return (NULL);
 	}
-	
 	for (i = 0; i < height; i++)
 	{
 		gridout[i] = malloc(width * sizeof(int));
@@ -34,10 +32,8 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			gridout[i][j] = 0;
-	
 	return (gridout);
 }
