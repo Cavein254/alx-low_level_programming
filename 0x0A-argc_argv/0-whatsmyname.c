fffea86b24c2e3b78bccd325d0c2dc736f2b956d
johnmuthua <caveomuthama@gmail.com>
2023-09-29 09:15:07 +0300

    Fixed betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - This program prints its name
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 for success
+ */
 int main(int argc, char **argv)
 {
 	printf("%s \n", argv[0]);
