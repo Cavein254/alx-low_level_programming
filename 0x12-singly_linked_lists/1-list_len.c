f6ebeff7b3ce3dc492d4ec5422f693b482c17ad8
johnmuthua <caveomuthama@gmail.com>
2023-10-19 21:57:13 +0300

    Add l-list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+/**
+ * list_len - A function to count number of elements in linked list
+ * @h: A pointer to the linked list
+ * Return: total elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		len += 1;
+		h = h->next;
+	}
+
+	return (len);
+}
