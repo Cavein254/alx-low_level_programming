12cb51f07141fbbad96ecf8d74f4e809d9daf78d
johnmuthua <caveomuthama@gmail.com>
2023-10-19 21:43:47 +0300

    cleared betty errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,16 +6,16 @@
 */
 size_t print_list(const list_t *h)
 {
-    size_t num = 0;
+	size_t num = 0;
 
-    while (h)
-    {
-        if (!h->str)
-            printf("[0] (nil) \n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        h = h->next;
-        num += 1;
-    }
-    return (num);
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil) \n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		num += 1;
+	}
+	return (num);
 }
