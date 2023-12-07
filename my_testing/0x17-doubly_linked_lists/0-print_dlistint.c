83e805976d3d2d73baaadd44ec2c284b72b551c3
johnmuthua <caveomuthama@gmail.com>
2023-12-07 21:16:07 +0300

    Add 0

---
+++ my_testing/0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h) {
+  size_t counter;
+
+  counter = 0;
+
+  /**Accounting when h is null */
+  if (h == NULL)
+    return (counter);
+  /**Accounting for backward*/
+  while (h->prev != NULL) {
+    counter++;
+    h = h->prev;
+  }
+  /**Accounting for forward*/
+  while (h != NULL) {
+    counter++;
+    printf("%d\n", h->n);
+    h = h->next;
+  }
+  return (counter);
+}
