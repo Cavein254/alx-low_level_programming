de2f250b2a29c68adac5a9e84486af39aad4bde2
John Muthua <caveomuthama@gmail.com>
2023-12-07 21:06:45 +0300

    Update 0-print_dlistint.c
    old changes back

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,22 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t counter;
+	int count;
 
-	counter = 0;
+	count = 0;
 
-	while(h != NULL)
-		{
-			counter = counter + 1;
-			h = h->next;
-		}
-	return (counter);
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
