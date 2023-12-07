f48b989aaf4a95f7fa555cf2c5cb287d0060bdc9
johnmuthua <caveomuthama@gmail.com>
2023-12-07 20:40:49 +0300

    Add all

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,29 +1,30 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_dlistint - prints number of elements in a node
- * @h: pointer to the provided list
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
  *
- * Return: size of the node
+ * @h: head of the list
+ * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h) {
-  int count;
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
 
-  count = 0;
+	count = 0;
 
-  if (h == NULL)
-    return (count);
+	if (h == NULL)
+		return (count);
 
-  while (h->prev != NULL)
-    h = h->prev;
+	while (h->prev != NULL)
+		h = h->prev;
 
-  while (h != NULL) {
-    printf("%d\n", h->n);
-    count++;
-    h = h->next;
-  }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
 
-  return (count);
+	return (count);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -7,13 +11,24 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- *
+ * for Holberton project
  */
-#include <stddef.h>
-typedef struct dlistint_s {
-  int n;
-  struct dlistint_s *prev;
-  struct dlistint_s *next;
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
+
 size_t print_dlistint(const dlistint_t *h);
-#endif // MAIN_H_
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+
+#endif
